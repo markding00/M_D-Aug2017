de52481276866a9a69345bf738f88d134236ef9f
markding00 <18dingk79@millshighsch.org>
2017-08-22 09:28:26 -0700

    Second Project -- using Git from Eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+
+// Mark Ding
+//My second Java project, it will compare print and println.
+//Auguts 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello, this is my second project.");
+		System.out.print("hahahahhah");
+		System.out.println(" lol");
+	}
+}
