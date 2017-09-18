f9e16da46bb3e647cd6e499da82645305217bd90
markding00 <18dingk79@millshighsch.org>
2017-09-18 08:05:20 -0700

    9/18

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (mark ding) 
+ * @version (a version number or a date)
+ */
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello World! From <mark ding>"); //Display the string
+	}
+
+}
