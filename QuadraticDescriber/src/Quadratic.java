70695f7e7c72a10d98f88dbe865dcf7cdbe32363
markding00 <31073703+markding00@users.noreply.github.com>
2017-09-26 09:04:05 -0700

    9/26

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,9 @@
+/*
+ * Mark Ding
+ * September 26, 2017
+ * This is a library that contains several methods that build up the description of the graph.
+ */
+
+public class Quadratic {
+
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,13 @@
+/*
+ * Mark Ding
+ * September 26, 2017
+ * This is the runner class for my quadratic methods.
+ */
+public class QuadraticClient {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
