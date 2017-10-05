0a0ecdcafe635d3598178e42e38b107aaa91110a
markding00 <18dingk79@millshighsch.org>
2017-10-05 08:15:01 -0700

    10/05

--- ProcessingNumbers/src/ProcessingNumbers.java
+++ ProcessingNumbers/src/ProcessingNumbers.java
@@ -16,6 +16,7 @@ public class ProcessingNumbers {
 		System.out.println("What are the numbers?");
 		next = userinput.nextInt();
 		
+		//initialization
 		int maximum = next;
 		int minimum = next;
 		int sumEven = next;
@@ -23,20 +24,30 @@ public class ProcessingNumbers {
 		
 		for(int i = 1; i < totalNumber; i++) {
 			next = userinput.nextInt();
+			
+			//This if statement can find the maximum in this string of numbers.
 			if(next > maximum) {
 				maximum = next;
 			}
+			
+			//This if statement can find the minimum in this string of numbers.
 			if(next < minimum) {
 				minimum = next;
 			}
+			
+			//This if statement can find the sum of all the even numbers.
 			if(next % 2 == 0) {
 				sumEven += next;
 			}
+			
+			//This if statement can find the largest even number in this string of numbers.
 			if(next > largestEven && next % 2 == 0) {
 				largestEven = next;
 			}
 			
 		}
+		
+		//These will print out max, min, sum of even numbers, max of even number.
 		System.out.println("Max: " + maximum);
 		System.out.println("Min: " + minimum);
 		System.out.println("The sum of all the even numbers is " + sumEven);
