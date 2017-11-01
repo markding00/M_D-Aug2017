c4a4a5fe34a24dbefb80d0ea58b8a1c9b1ead699
markding00 <18dingk79@millshighsch.org>
2017-11-01 11:23:36 -0700

    10/31

---
+++ LotsOfCopies/src/LotsOfCopies.java
@@ -0,0 +1,18 @@
+/*
+ * Mark Ding
+ * Ms.Dreyer
+ * APCS1 10/31/2017
+ */
+public class LotsOfCopies {
+
+	public static void main(String[] args) {
+		int num = 1;
+		String strMain = "APCS";
+		int[] arrMain = {1, 2, 3, 4, 5};
+	}
+	
+	public static void changeMe(int x, String str, int[] arr) {
+		
+	}
+
+}
