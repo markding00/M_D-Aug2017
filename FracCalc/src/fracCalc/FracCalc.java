80b50175b1f81ebba752a214fb58c8641d243105
markding00 <18dingk79@millshighsch.org>
2017-11-08 11:31:33 -0800

    11/08

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -12,10 +12,15 @@ public class FracCalc {
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
     	Scanner userInput = new Scanner(System.in);
-    	String formula = userInput.nextLine();
-    	String answer = produceAnswer(formula);
-    	System.out.println(answer);
-
+    	boolean done = false;
+    	while(!done) {
+    		String formula = userInput.nextLine();
+        	String answer = produceAnswer(formula);
+        	System.out.println(answer);
+        	if(formula.equals("quit")) {
+        		done = true;
+        	}
+    	}
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -30,8 +35,27 @@ public class FracCalc {
     { 
         // TODO: Implement this function to produce the solution to the input
     	String[] operand = input.split(" ");
+        String answer;
+        String wholeNum = "", numerator = "", denominator = "";
+        int underScore = operand[2].indexOf("_");
+     	int slash = operand[2].indexOf("/");
         
-        return operand[2];
+        if(operand[2].indexOf("_") >= 0) {
+        	wholeNum = operand[2].substring(0, underScore);
+        	numerator = operand[2].substring(underScore + 1, slash);
+        	denominator = operand[2].substring(slash + 1, operand[2].length());
+        } else if (operand[2].indexOf("_") < 0 && operand[2].indexOf("/") >= 0) {
+        	wholeNum = "0";
+        	numerator = operand[2].substring(0, slash);
+        	denominator = operand[2].substring(slash + 1, operand[2].length());
+        } else if (operand[2].indexOf("_") < 0 && operand[2].indexOf("/") < 0) {
+        	wholeNum = operand[2];
+        	numerator = "0";
+        	denominator = "1";
+        }
+        
+        answer = "whole:" + wholeNum + " numerator:" + numerator + " denominator:" + denominator;
+        return answer;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
