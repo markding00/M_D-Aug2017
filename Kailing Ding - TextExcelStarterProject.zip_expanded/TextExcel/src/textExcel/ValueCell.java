f9af0363b0993bb7d96e925f59863da43053c434
markding00 <31073703+markding00@users.noreply.github.com>
2018-03-21 10:12:01 -0700

    03/20

--- Kailing Ding - TextExcelStarterProject.zip_expanded/TextExcel/src/textExcel/ValueCell.java
+++ Kailing Ding - TextExcelStarterProject.zip_expanded/TextExcel/src/textExcel/ValueCell.java
@@ -10,17 +10,9 @@ public class ValueCell extends RealCell {
 		if(cellText.length() < 10) {
 			if(!cellText.contains(".")) {    //checks if its a whole number 
 				cellText += ".0";
-			}else if(cellText.charAt(cellText.length() -1) == 0 && cellText.indexOf(".") != cellText.length() - 2) {       
+			}else if(cellText.charAt(cellText.length() -1) == 0 && cellText.indexOf(".") != cellText.length() - 2){       
 				//reverse for loop to check for two or more zeros together from the end
-				int count = 0;
-				for(int i = cellText.length() - 1; i >= 0; i--) {
-					if(cellText.charAt(i) == 0) {
-						count++;
-						if(count >= 2){
-							cellText = cellText.substring(0, i);
-						}
-					}
-				}
+				delete0(cellText);
 			}
 			return addSpaces(cellText);
 		}else {	
@@ -34,5 +26,19 @@ public class ValueCell extends RealCell {
 	
 	public double getDoubleValue() {
 		return Double.parseDouble(getRealCell());
+	}
+	
+	//this method can delete all the 0s behind the last valuable decimal number. 
+	public String delete0(String cellText) {
+		int count = 0;
+		for(int i = cellText.length() - 1; i >= 0; i--) {
+			if(cellText.charAt(i) == 0) {
+				count++;
+				if(count >= 2){
+					cellText = cellText.substring(0, i);
+				}
+			}
+		}
+		return cellText;
 	}
 }
