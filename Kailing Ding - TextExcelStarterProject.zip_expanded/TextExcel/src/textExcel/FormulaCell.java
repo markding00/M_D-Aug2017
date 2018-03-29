877e4c7fb1400ce8e6c91c3ab76fa143ade19928
markding00 <31073703+markding00@users.noreply.github.com>
2018-03-29 00:27:51 -0700

    03/28

--- Kailing Ding - TextExcelStarterProject.zip_expanded/TextExcel/src/textExcel/FormulaCell.java
+++ Kailing Ding - TextExcelStarterProject.zip_expanded/TextExcel/src/textExcel/FormulaCell.java
@@ -9,17 +9,16 @@ public class FormulaCell extends RealCell{
 	public String abbreviatedCellText() {
 		String cellText = getDoubleValue() + "";
 		if(cellText.length() < 10) {
-			addSpaces(cellText);
+			return addSpaces(cellText);
 		}else {
-			cellText = cellText.substring(0,10);
+			return cellText.substring(0,10);
 		}
-		return cellText;
 	}
 	
 	public double getDoubleValue() {
 		String[] formula = getRealCell().split(" ");
 		double result = Double.parseDouble(formula[1]);
-		for(int i = 2; i < formula.length - 3; i += 2) {
+		for(int i = 2; i < formula.length - 2; i += 2) {
 			if(formula[i].equals("+")) {
 				result += Double.parseDouble(formula[i+1]);
 			}else if(formula[i].equals("-")) {
@@ -37,7 +36,7 @@ public class FormulaCell extends RealCell{
 
 	@Override
 	public String fullCellText() {
-		return getDoubleValue() + "";
+		return getRealCell();
 	}
 	
 	
